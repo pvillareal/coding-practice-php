4cf27cf6b8586e8e68505cbbe03c8eeefb207afe
Philippe <villareal.philippe@gmail.com>
2024-02-19 21:37:32 -0800

    rector changes

---
+++ rector.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\SetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/test',
+    ])
+    // uncomment to reach your current PHP version
+    // ->withPhpSets()
+    ->withSets([
+        SetList::CODE_QUALITY,
+        SetList::PHP_83,
+    ]);
